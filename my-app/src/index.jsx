593d9a5997767b873fe4bebc557ab3aa4be9e758
tibocst <thibault.coste.233@gmail.com>
2023-08-07 17:26:35 +0200

    Etape 3 : router et home + a propos + error

---
+++ my-app/src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Propos from './pages/Propos'
+import Header from './components/Header'
+import Error from './components/Error'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/propos" element={<Propos />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
