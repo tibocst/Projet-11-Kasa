513213b123a6c25a3023a41e0a71e44c7914d5bb
tibocst <thibault.coste.233@gmail.com>
2023-08-14 12:53:38 +0200

    ajout compteur sur slideshow

--- my-app/src/components/Slideshow/index.jsx
+++ my-app/src/components/Slideshow/index.jsx
@@ -9,13 +9,16 @@ function Slideshow(props) {
 
   const [display, setDisplay] = useState(0)
 
+  console.log(pictures.length)
+
   return (
     <div className='slideshow'>
-      <img className='slideshow-chevron_gauche' onClick={() => display === 0 ? setDisplay(document.querySelectorAll('.slideshow-div-img').length) : setDisplay(display - 1)} src={CHEVRON} alt="chevron" />
+      <img className='slideshow-chevron_gauche' onClick={() => display === 0 ? setDisplay(pictures.length-1) : setDisplay(display - 1)} src={CHEVRON} alt="chevron" />
       <div className='slideshow-div'>
         {pictures.map((picture, i) => <img className={display === i ? 'slideshow-div-img_display' : 'slideshow-div-img'} src={picture} key={i} alt="slideshow" />)}
+        <p className='slideshow-compteur'>{ (display+1) + "/" + pictures.length}</p>
       </div>
-      <img className='slideshow-chevron_droit' onClick={() => document.querySelectorAll('.slideshow-div-img').length === (display) ? setDisplay(0) : setDisplay(display + 1)} src={CHEVRON} alt="chevron" />
+      <img className='slideshow-chevron_droit' onClick={() => pictures.length-1 === display ? setDisplay(0) : setDisplay(display + 1)} src={CHEVRON} alt="chevron" />
     </div>
   );
 }
