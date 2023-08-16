22d544d0273e115395cecaad77df48e80b907cf4
tibocst <thibault.coste.233@gmail.com>
2023-08-16 17:16:24 +0200

    modif carousel test animation

--- my-app/src/components/Slideshow/index.jsx
+++ my-app/src/components/Slideshow/index.jsx
@@ -13,9 +13,9 @@ function Slideshow(props) {
     <div className='slideshow'>
       <img className={pictures.length === 1 ? 'slideshow-none' : 'slideshow-chevron_gauche'} onClick={() => display === 0 ? setDisplay(pictures.length-1) : setDisplay(display - 1)} src={CHEVRON} alt="chevron" />
       <div className='slideshow-div'>
-        {pictures.map((picture, i) => <img className={display === i ? 'slideshow-div-img_display' : 'slideshow-div-img'} src={picture} key={i} alt="slideshow" />)}
-        <p className={pictures.length === 1 ? 'slideshow-none' : 'slideshow-compteur'}>{ (display+1) + "/" + pictures.length}</p>
+        {pictures.map((picture, i) => <img className={display === i ? 'slideshow-div-img_display' : 'slideshow-div-img'} src={picture} key={i} alt="slideshow"/>)}
       </div>
+      <p className={pictures.length === 1 ? 'slideshow-none' : 'slideshow-compteur'}>{ (display+1) + "/" + pictures.length}</p>
       <img className={pictures.length === 1 ? 'slideshow-none' : 'slideshow-chevron_droit'} onClick={() => pictures.length-1 === display ? setDisplay(0) : setDisplay(display + 1)} src={CHEVRON} alt="chevron" />
     </div>
   );
